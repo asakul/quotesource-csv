246b11664252b3fd227f9c1b6e4c6be47553df14
Denis Tereshkin <denis@kasan.ws>
2015-10-05 22:27:55 +0600

    feature(*): command line arguments parsing

--- quotesource-csv.py
+++ quotesource-csv.py
@@ -1,6 +1,20 @@
+#!/usr/bin/env python
 '''
 
 '''
+
+import argparse
+import zmq
+from eventloop import EventLoop
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser(description='Quotesource that streams quotes from csv-files')
+    parser.add_argument('--exchange-id', help='Specifies exchange ID for all streams', required=True)
+    parser.add_argument('--control-ep', help='Specifies control endpoint', required=True)
+    parser.add_argument('--stream-ep', help='Specifies stream endpoint', required=True)
+    
+    args = parser.parse_args()
+    
+    ctx = zmq.Context.instance()
+    loop = EventLoop(ctx, args.control_ep, args.stream_ep, args.exchange_id)
+    loop.start()
